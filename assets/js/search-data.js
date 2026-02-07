afd4cb6b3797cba6e870a598609a011d62c9ca3f
profhannahchang <181260513+profhannahchang@users.noreply.github.com>
2026-02-07 16:36:05 +0000

    Deploying to gh-pages from @ profhannahchang/profhannahchang.github.io@69eeea368b9d8c829e96f0076f8caa56948fe00c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-teaching",
           title: "teaching",
-          description: "",
+          description: "Courses taught, guest lectures, and doctoral dissertation supervision.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
